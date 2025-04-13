16f230d366e697193f19ad8916b7f3f7df8aa870
naokirin <naoki.rin186@gmail.com>
2025-04-13 13:48:33 +0900

    Add 1 week ago metrics

--- .github/scripts/send_metrics_to_slack.rb
+++ .github/scripts/send_metrics_to_slack.rb
@@ -9,30 +9,16 @@ require 'rubycritic/analysers/coverage'
 require 'churn/calculator'
 require 'json'
 require 'slack-notifier'
-
-def score(analyser_name)
-  smell_count = analysed_modules.map { |m| m.smells.select { |smell| smell.analyser == analyser_name }.size }.compact.sum
-  smell_count * 1.0 / analysed_modules.count rescue 0
-end
-
-# Slack通知用のWebhook URL
-slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
-notifier = Slack::Notifier.new(slack_webhook_url)
+require 'git'
 
 # 日付
 today = Time.now.strftime('%Y-%m-%d')
 
 # 結果を格納するハッシュ
-results = {
-  date: today,
-  metrics: {}
-}
+results = []
 
 # 対象のパス
 target_paths = %w[app lib]
-
-# RubyCriticのメトリクスを実行（Churnは除く）
-puts "Running RubyCritic metrics..."
 
 ANALYSERS =[
   RubyCritic::Analyser::FlaySmells,
@@ -42,67 +28,113 @@ ANALYSERS =[
   RubyCritic::Analyser::Attributes,
   RubyCritic::Analyser::Coverage
 ]
+working_dir = File.expand_path('../../..', __FILE__)
+git = Git.open(working_dir, :log => Logger.new(STDOUT))
 
-analysed_modules = RubyCritic::AnalysedModulesCollection.new(target_paths)
-ANALYSERS.each do |analyser_class|
-  analyser_instance = analyser_class.new(analysed_modules)
-  puts "running #{analyser_instance}"
-  analyser_instance.run
+def smell_score(analyser_name, analysed_modules)
+  smell_count = analysed_modules.map { |m| m.smells.select { |smell| smell.analyser == analyser_name }.size }.compact.sum
+  smell_count * 1.0 / analysed_modules.count rescue 0
 end
 
-# RubyCriticからのデータ集計
-flog_score = score('flog')
-flay_score = score('flay')
-complexity = analysed_modules.map(&:complexity).compact.sum / analysed_modules.count rescue 0
-smells_count = analysed_modules.map { |m| m.smells.count }.sum
+def complexity_score(analysed_modules)
+  analysed_modules.map(&:complexity).compact.sum / analysed_modules.count rescue 0
+end
 
-results[:metrics] = {
-  average_flog_score: flog_score.round(2),
-  average_flay_score: flay_score.round(2),
-  average_complexity: complexity.round(2),
-  total_code_smells: smells_count
-}
+def metrics(git, target_paths, date_string)
+  commit_hash = ''
+  if date_string == 'today'
+    commit_hash = git.log(1).first.sha
+  else
+    commit_hash = git.log.since(date_string).last.sha
+  end
+  git.checkout(commit_hash) # 指定した日付のコミットにチェックアウト
 
-# 別途Churn gemで計算
-# 1ヶ月分のChurnデータを取得
-puts "Running Churn analysis..."
-churn = Churn::ChurnCalculator.new({
-  minimum_churn_count: 3,
-  start_date: (Date.today - 30).to_s
-})
-churn_result = churn.report(false)
+  # RubyCriticのメトリクスを実行（Churnは除く）
+  puts "Running RubyCritic metrics..."
 
-# Churnデータを集計
-churn_classes = churn_result[:churn][:classes] || []
-churn_methods = churn_result[:churn][:methods] || []
+  result = { date: date_string, metrics: {} }
 
-top_churned_files = churn_classes.sort_by { |file| -file[:times_changed] }.first(5)
-top_churned_methods = churn_methods.sort_by { |method| -method[:times_changed] }.first(5)
+  analysed_modules = RubyCritic::AnalysedModulesCollection.new(target_paths)
+  ANALYSERS.each do |analyser_class|
+    analyser_instance = analyser_class.new(analysed_modules)
+    puts "running #{analyser_instance}"
+    analyser_instance.run
+  end
 
-results[:metrics][:churn] = {
-  average_class_churn: (churn_classes.empty? ? 0 : churn_classes.map { |c| c[:times_changed] }.sum / churn_classes.size.to_f).round(2),
-  top_churned_files: top_churned_files.map { |f| "#{f[:file_path]} (#{f[:times_changed]}回変更)" },
-  top_churned_methods: top_churned_methods.map { |m| "#{m[:method_name]} in #{m[:file_path]} (#{m[:times_changed]}回変更)" }
-}
+  # RubyCriticからのデータ集計
+  flog_score = smell_score('flog', analysed_modules)
+  flay_score = smell_score('flay', analysed_modules)
+  complexity = complexity_score(analysed_modules)
+  smells_count = analysed_modules.map { |m| m.smells.count }.sum
+
+  result[:metrics] = {
+    average_flog_score: flog_score.round(2),
+    average_flay_score: flay_score.round(2),
+    average_complexity: complexity.round(2),
+    total_code_smells: smells_count
+  }
+
+  # 別途Churn gemで計算
+  # 1ヶ月分のChurnデータを取得
+  puts "Running Churn analysis..."
+  churn = Churn::ChurnCalculator.new({
+    minimum_churn_count: 3,
+    start_date: (Date.today - 30).to_s
+  })
+  churn_result = churn.report(false)
+
+  # Churnデータを集計
+  churn_classes = churn_result[:churn][:classes] || []
+  churn_methods = churn_result[:churn][:methods] || []
+
+  top_churned_files = churn_classes.sort_by { |file| -file[:times_changed] }.first(5)
+  top_churned_methods = churn_methods.sort_by { |method| -method[:times_changed] }.first(5)
+
+  result[:metrics][:churn] = {
+    average_class_churn: (churn_classes.empty? ? 0 : churn_classes.map { |c| c[:times_changed] }.sum / churn_classes.size.to_f).round(2),
+    top_churned_files: top_churned_files.map { |f| "#{f[:file_path]} (#{f[:times_changed]}回変更)" },
+    top_churned_methods: top_churned_methods.map { |m| "#{m[:method_name]} in #{m[:file_path]} (#{m[:times_changed]}回変更)" }
+  }
+
+  result
+end
+
+results << metrics(git, target_paths, 'today')
+results << metrics(git, target_paths, '1 week ago')
+
+# Slack通知用のWebhook URL
+slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
+notifier = Slack::Notifier.new(slack_webhook_url)
+
+result_today = results.find { |r| r[:date] == 'today' }
+result_last_week = results.find { |r| r[:date] == '1 week ago' }
 
 # Slack通知用のメッセージ作成
 message = <<~MESSAGE
   :chart_with_upwards_trend: *コードメトリクスレポート (#{today})* :chart_with_upwards_trend:
 
   *基本メトリクス:*
-  • 平均Flogカウント: #{results[:metrics][:average_flog_score]}
-  • 平均Flayカウント: #{results[:metrics][:average_flay_score]}
-  • 平均複雑度: #{results[:metrics][:average_complexity]}
-  • コードスメル数: #{results[:metrics][:total_code_smells]}
+  • 平均Flogカウント: #{result_today[:metrics][:average_flog_score]}（前週：#{result_last_week[:metrics][:average_flog_score]}）
+  • 平均Flayカウント: #{result_today[:metrics][:average_flay_score]}（前週：#{result_last_week[:metrics][:average_flay_score]}）
+  • 平均複雑度: #{result_today[:metrics][:average_complexity]}（前週：#{result_last_week[:metrics][:average_complexity]}）
+  • コードスメル数: #{result_today[:metrics][:total_code_smells]}（前週：#{result_last_week[:metrics][:total_code_smells]}）
 
   *Churn (コード変更頻度):*
-  • 平均クラス変更頻度: #{results[:metrics][:churn][:average_class_churn]}
+  • 平均クラス変更頻度: #{result_today[:metrics][:churn][:average_class_churn]}（前週：#{result_last_week[:metrics][:churn][:average_class_churn]}）
   
   *最も変更の多いファイル:*
-  #{results[:metrics][:churn][:top_churned_files].map { |f| "• #{f}" }.join("\n")}
+    今週
+    #{result_today[:metrics][:churn][:top_churned_files].map { |f| "• #{f}" }.join("\n")}
+  
+    前週
+    #{result_last_week[:metrics][:churn][:top_churned_files].map { |f| "• #{f}" }.join("\n")}
   
   *最も変更の多いメソッド:*
-  #{results[:metrics][:churn][:top_churned_methods].map { |m| "• #{m}" }.join("\n")}
+    今週
+    #{result_today[:metrics][:churn][:top_churned_methods].map { |m| "• #{m}" }.join("\n")}
+  
+    前週
+    #{result_last_week[:metrics][:churn][:top_churned_methods].map { |m| "• #{m}" }.join("\n")}
 MESSAGE
 
 # Slackに通知
